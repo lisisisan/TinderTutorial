e76a5f61cd3072137755e237588605878fa50987
lisisian <125748777+lisisisan@users.noreply.github.com>
2025-06-12 13:08:13 +0500

    Initial Commit

---
+++ TinderTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TinderTutorial
+//
+//  Created by Ilona on 6/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TinderTutorial/TinderTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderTutorialApp.swift
+//  TinderTutorial
+//
+//  Created by Ilona on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TinderTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
